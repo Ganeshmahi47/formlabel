6cf9b82bccbcbb79c0ebd272e681d70dd796ef79
Ganeshmahi47 <108626667+Ganeshmahi47@users.noreply.github.com>
2022-09-01 15:04:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function foo(){
+    var a =document.getElementById("firstname").value
+console.log(a)
+var b =document.getElementById("midlename").value
+console.log(b)
+var c =document.getElementById("lastname").value
+console.log(c)
+var d =document.getElementById("email").value
+console.log(d)
+
+}
